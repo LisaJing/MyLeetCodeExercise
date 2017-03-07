e7479a8dbe9e0c4c20d2d1c39a2e485d9640f1cd
LisaJing <lisajing_go@126.com>
2017-03-07 09:55:49 +0800

    lisa commit 111 problem 20170307

---
+++ 111 Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL)
+        	return 0;
+        if(root -> left == NULL && root -> right == NULL)
+        	return 1;
+        if(root -> left == NULL)
+        	return 1 + minDepth(root -> right);
+        else if(root -> right == NULL)
+        	return 1 + minDepth(root -> left);
+        else
+        	return 1 + std::min(minDepth(root -> left) , minDepth(root -> right)); 
+    }
+};
